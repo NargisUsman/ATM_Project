9941d3d695f5a2d55ded970c760c1eb0b03de345
sip <dspirin@me.com>
2020-11-28 10:24:15 -0500

    Merged

--- src/main/java/ATM/DepositMenu.java
+++ src/main/java/ATM/DepositMenu.java
@@ -24,13 +24,13 @@ public class DepositMenu {
         int option = scan.nextInt();
         switch(option) {
             case 1:
-                DepositMenu.checkingOptions();
+                //DepositMenu.checkingOptions();
                 break;
             case 2:
-                DepositMenu.savingsOptions();
+                //DepositMenu.savingsOptions();
                 break;
             case 3:
-                DepositMenu.moneyMarketOptions();
+                //DepositMenu.moneyMarketOptions();
                 break;
             case 4:
                 //MainMenu.menu();
@@ -44,96 +44,96 @@ public class DepositMenu {
 
     }
 
-    public static void checkingOptions() {
-        if (DataBase.readExcelFile() > 1) {
-            System.out.println("Which account to deposit?");
-            for (int i = 0; i <= DataBase.lengthOfList(); i++) {
-                System.out.println("Checking account" + DataBase.readExcelFile());
-            }
-        } else {
-            System.out.println("Enter the amount you like to deposit");
-            Scanner scan = new Scanner(System.in);
-            int amount = scan.nextInt();
-            if (amount > 0) {
-                System.out.println("How do you want to deposit?\n1 Cash\n2 Check");
-                Scanner scan1 = new Scanner(System.in);
-                int result = scan1.nextInt();
-                if (result == 1) {
-                    balance += amount;
-                } else {
-                    System.out.println("Enter the check number");
-                    Scanner scan2 = new Scanner(System.in);
-                    String checkNum = scan2.nextLine();
-                    DataBase.writeExcelFile(checkNum, 0, 1);
-                    System.out.println("Enter the amount");
-                    Scanner scan3 = new Scanner(System.in);
-                    int checkAmount = scan3.nextInt();
-                    balance += checkAmount;
-                }
-            }
-        }
-    }
-
-    public static void savingsOptions() {
-        if(DataBase.readExcelFile()>1) {
-            System.out.println("Which account to deposit?");
-            for(int i =0; i<=DataBase.lengthOfList(); i++) {
-                System.out.println("Savings account" + DataBase.readExcelFile());
-            }
-        } else {
-            System.out.println("Enter the amount you like to deposit");
-            Scanner scan = new Scanner(System.in);
-            int amount = scan.nextInt();
-            if(amount>0) {
-                System.out.println("How do you want to deposit?\n1 Cash\n2 Check");
-                Scanner scan1 = new Scanner(System.in);
-                int result = scan1.nextInt();
-                if(result==1) {
-                    balance+=amount;
-                } else {
-                    System.out.println("Enter the check number");
-                    Scanner scan2 = new Scanner(System.in);
-                    String checkNum = scan2.nextLine();
-                    DataBase.writeExcelFile(checkNum, 0, 1);
-                    System.out.println("Enter the amount");
-                    Scanner scan3 = new Scanner(System.in);
-                    int checkAmount = scan3.nextInt();
-                    balance += checkAmount;
-                }
-            }
-        }
-
-    }
-
-    public static void moneyMarketOptions() {
-        if(DataBase.readExcelFile()>1) {
-            System.out.println("Which account to deposit?");
-            for(int i =0; i<=DataBase.lengthOfList(); i++) {
-                System.out.println("Money Market account" + DataBase.readExcelFile());
-            }
-        } else {
-            System.out.println("Enter the amount you like to deposit");
-            Scanner scan = new Scanner(System.in);
-            int amount = scan.nextInt();
-            if(amount>0) {
-                System.out.println("How do you want to deposit?\n1 Cash\n2 Check");
-                Scanner scan1 = new Scanner(System.in);
-                int result = scan1.nextInt();
-                if(result==1) {
-                    balance+=amount;
-                } else {
-                    System.out.println("Enter the check number");
-                    Scanner scan2 = new Scanner(System.in);
-                    String checkNum = scan2.nextLine();
-                    DataBase.writeExcelFile(checkNum, 0, 1);
-                    System.out.println("Enter the amount");
-                    Scanner scan3 = new Scanner(System.in);
-                    int checkAmount = scan3.nextInt();
-                    balance += checkAmount;
-                }
-            }
-        }
-    }
+//    public static void checkingOptions() {
+//        if (DataBase.readExcelFile() > 1) {
+//            System.out.println("Which account to deposit?");
+//            for (int i = 0; i <= DataBase.lengthOfList(); i++) {
+//                System.out.println("Checking account" + DataBase.readExcelFile());
+//            }
+//        } else {
+//            System.out.println("Enter the amount you like to deposit");
+//            Scanner scan = new Scanner(System.in);
+//            int amount = scan.nextInt();
+//            if (amount > 0) {
+//                System.out.println("How do you want to deposit?\n1 Cash\n2 Check");
+//                Scanner scan1 = new Scanner(System.in);
+//                int result = scan1.nextInt();
+//                if (result == 1) {
+//                    balance += amount;
+//                } else {
+//                    System.out.println("Enter the check number");
+//                    Scanner scan2 = new Scanner(System.in);
+//                    String checkNum = scan2.nextLine();
+//                    DataBase.writeExcelFile(checkNum, 0, 1);
+//                    System.out.println("Enter the amount");
+//                    Scanner scan3 = new Scanner(System.in);
+//                    int checkAmount = scan3.nextInt();
+//                    balance += checkAmount;
+//                }
+//            }
+//        }
+//    }
+//
+//    public static void savingsOptions() {
+//        if(DataBase.readExcelFile()>1) {
+//            System.out.println("Which account to deposit?");
+//            for(int i =0; i<=DataBase.lengthOfList(); i++) {
+//                System.out.println("Savings account" + DataBase.readExcelFile());
+//            }
+//        } else {
+//            System.out.println("Enter the amount you like to deposit");
+//            Scanner scan = new Scanner(System.in);
+//            int amount = scan.nextInt();
+//            if(amount>0) {
+//                System.out.println("How do you want to deposit?\n1 Cash\n2 Check");
+//                Scanner scan1 = new Scanner(System.in);
+//                int result = scan1.nextInt();
+//                if(result==1) {
+//                    balance+=amount;
+//                } else {
+//                    System.out.println("Enter the check number");
+//                    Scanner scan2 = new Scanner(System.in);
+//                    String checkNum = scan2.nextLine();
+//                    DataBase.writeExcelFile(checkNum, 0, 1);
+//                    System.out.println("Enter the amount");
+//                    Scanner scan3 = new Scanner(System.in);
+//                    int checkAmount = scan3.nextInt();
+//                    balance += checkAmount;
+//                }
+//            }
+//        }
+//
+//    }
+//
+//    public static void moneyMarketOptions() {
+//        if(DataBase.readExcelFile()>1) {
+//            System.out.println("Which account to deposit?");
+//            for(int i =0; i<=DataBase.lengthOfList(); i++) {
+//                System.out.println("Money Market account" + DataBase.readExcelFile());
+//            }
+//        } else {
+//            System.out.println("Enter the amount you like to deposit");
+//            Scanner scan = new Scanner(System.in);
+//            int amount = scan.nextInt();
+//            if(amount>0) {
+//                System.out.println("How do you want to deposit?\n1 Cash\n2 Check");
+//                Scanner scan1 = new Scanner(System.in);
+//                int result = scan1.nextInt();
+//                if(result==1) {
+//                    balance+=amount;
+//                } else {
+//                    System.out.println("Enter the check number");
+//                    Scanner scan2 = new Scanner(System.in);
+//                    String checkNum = scan2.nextLine();
+//                    DataBase.writeExcelFile(checkNum, 0, 1);
+//                    System.out.println("Enter the amount");
+//                    Scanner scan3 = new Scanner(System.in);
+//                    int checkAmount = scan3.nextInt();
+//                    balance += checkAmount;
+//                }
+//            }
+//        }
+//    }
 
     public void printReceipt () {
         System.out.println("Do you want to print the transaction history?");
